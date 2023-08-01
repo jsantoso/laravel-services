14a039be7a08e29c6e7d1f56d8556ed9c1ab0824
Jeffrey Santoso <jeffey.k.santoso@gmail.com>
2023-08-01 10:45:40 +1000

    Added a way to disable self-test by reading from config file

--- src/SelfTest/routes/api.php
+++ src/SelfTest/routes/api.php
@@ -3,6 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use Jsantoso\LaravelServices\SelfTest\Controllers\SelfTestController;
 
-Route::middleware(['api'])->group(function(){   
-    Route::get('selfTestResult' , [SelfTestController::class, 'getResult']);
+Route::middleware(['api'])->group(function () {
+    
+    $config = config('selftest');
+    if (
+        array_key_exists('enabled', $config) &&
+        !$config['enabled']
+    ) {
+        $enabled = false;
+    } else {
+        $enabled = true;
+    }
+    
+    if ($enabled) {
+        Route::get('selfTestResult', [SelfTestController::class, 'getResult']);
+    }
 });
--- src/SelfTest/routes/web.php
+++ src/SelfTest/routes/web.php
@@ -3,6 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use Jsantoso\LaravelServices\SelfTest\Controllers\SelfTestController;
 
-Route::middleware(['web'])->group(function(){   
-    Route::get('selftest' , [SelfTestController::class, 'getTestData']);
+Route::middleware(['web'])->group(function () {
+    
+    $config = config('selftest');
+    if (
+        array_key_exists('enabled', $config) &&
+        !$config['enabled']
+    ) {
+        $enabled = false;
+    } else {
+        $enabled = true;
+    }
+    
+    if ($enabled) {
+        Route::get('selftest', [SelfTestController::class, 'getTestData']);
+    }
 });
